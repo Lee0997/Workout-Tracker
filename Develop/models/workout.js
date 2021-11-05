7444a009dd32938b392820b7cbca581b00bf91c5
Lee <leenock2012@yahoo.co.uk>
2021-11-05 21:46:52 +0000

    Populated API routes and developed Model

---
+++ Develop/models/workout.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  exercises: [
+    {
+      type: { String, required: "type required" },
+      name: {
+        type: String,
+        required: "name required",
+      },
+      duration: {
+        type: Number,
+        required: "duration required",
+      },
+      reps: Number,
+      distance: Number,
+      sets: Number,
+      weight: Number,
+    },
+  ],
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
